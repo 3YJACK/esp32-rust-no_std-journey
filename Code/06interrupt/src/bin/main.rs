01afa1b5052c91baed89edbd9d85d4620ca0bb5a
Jaeger <2190088763@qq.com>
2026-08-10 14:43:51 +0800

    [FEAT] add 06interrupt example

--- Code/06interrupt/src/bin/main.rs
+++ Code/06interrupt/src/bin/main.rs
@@ -7,10 +7,16 @@
 )]
 #![deny(clippy::large_stack_frames)]
 
+use core::cell::RefCell;
+
+use critical_section::Mutex;
+
 use esp_hal::{
     clock::CpuClock,
     main,
     time::{Duration, Instant},
+    gpio::*,
+
 };
 
 use log::info;
@@ -22,6 +28,10 @@ extern crate alloc;
 // This creates a default app-descriptor required by the esp-idf bootloader.
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
 esp_bootloader_esp_idf::esp_app_desc!();
+
+// 为了让中断处理程序能够访问，定义为全局变量，再包装为Mutex临界区互斥锁和RefCell内部可变器，同时使用Option包装Input类型，进行延迟初始化
+// 因为static变量在编译时就是确定值，而Input类型的对象实例化在运行时才能完成，所以先使用Option类型包装并赋值为None进行占位，完成实例化后再替换
+static BUTTON: Mutex<RefCell<Option<Input>>> = Mutex::new(RefCell::new(None));
 
 #[allow(
     clippy::large_stack_frames,
@@ -56,15 +66,59 @@ fn main() -> ! {
     let _ = peripherals.GPIO36;
     let _ = peripherals.GPIO37;
 
-
     esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 73744);
 
+    // 创建IO引脚管理器并设置中断处理程序
+    let mut io = Io::new(peripherals.IO_MUX);
+    io.set_interrupt_handler(handler);
+
+    let mut led = Output::new(peripherals.GPIO4, Level::Low, OutputConfig::default());
+
+    let config = InputConfig::default().with_pull(Pull::Up);
+    let mut button = Input::new(peripherals.GPIO0, config);
+
+    critical_section::with(|cs| {
+        // 设置按钮引脚为下降沿触发中断
+        button.listen(Event::FallingEdge);
+        // 将BUTTON全局变量替换为实例化的button对象，这样中断处理程序就可以访问到按钮引脚
+        BUTTON.borrow_ref_mut(cs)   // 获取BUTTON的临界区互斥锁并借用可变引用
+              .replace(button)
+    });
 
     loop {
-        info!("Hello world!");
-        let delay_start = Instant::now();
-        while delay_start.elapsed() < Duration::from_millis(500) {}
+
     }
 
     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.1.0/examples
+}
+
+#[handler]
+// 中断处理程序，必须使用#[ram]属性将其放置在RAM中，以便在中断发生时能够快速响应
+#[ram]
+fn handler() {
+    if critical_section::with(|cs| {
+        BUTTON
+            // borrow_ref_mut返回的RefMut<Option<Input>>类型，这是一个指向Option的智能指针，
+            // as_mut()方法从RefMut<Option<Input>>中取出Option<&mut Input>，
+            // unwrap()方法解封装Option，如果Option是Some，则返回其中的值&mut Input，如果是None，则会panic。            
+            .borrow_ref_mut(cs)
+            .as_mut()   
+            .unwrap()
+            // 查看BUTTON引脚是否触发了中断
+            .is_interrupt_set()
+    }) {
+        led.toggle();
+        info!("Button was the source of the interrupt");
+    } else {
+        info!("Button was not the source of the interrupt");
+    }
+
+    critical_section::with(|cs| {
+        BUTTON
+            .borrow_ref_mut(cs)
+            .as_mut()   
+            .unwrap()
+            // 与is_interrupt_set()方法配合成对使用，清除中断标志位，防止中断处理程序被重复触发
+            .clear_interrupt()
+    });
 }
